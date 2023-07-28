27d0f63455f5f95dc34fffd2c274451be74e6788
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-28 09:32:11 -0600

    add bootstrap_set_up.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Import Bootstrap JavaScript components
+import 'bootstrap';
+
+// Import Bootstrap CSS styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "@hotwired/turbo-rails"
 import "controllers"
