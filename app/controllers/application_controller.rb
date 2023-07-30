a8c724b3214413de2d806355e8f6d0a938895a38
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-30 14:42:11 -0600

    add require_login method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def require_login
+    unless current_user
+      flash[:alert] = "You must be logged in, to access the page you requested."
+      redirect_to root_path
+    end
+  end
 end
