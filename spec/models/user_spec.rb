e6d684f7eb5633435406714c181b128dfa240e41
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-29 18:35:56 -0600

    add test for valid user factory.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid factory' do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
+  end
 end
