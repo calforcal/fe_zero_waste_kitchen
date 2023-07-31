3c8c4b7c1da9b0057762cfccb944ed0d5d636dda
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-31 13:02:47 -0600

    comment out code to create user to the BE for now.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,7 +20,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
+    # require 'pry'; binding.pry
+    # # Make a call to the BE API and check if this user already exists, if yes do line 28 if no do 26-27 first.
+    # facade = UserFacade.new(resource_or_scope)
+    # facade.create_user(resource_or_scope)
     stored_location_for(resource_or_scope) || user_path(resource_or_scope.id)
+    #create_user
+    #redirect_path
   end
 
   private
