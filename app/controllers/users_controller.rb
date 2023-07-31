ef505e76032c644f712e609690314ca046ce7dd8
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-31 13:02:21 -0600

    comment out create action for now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
     @facade = UserFacade.new(params)
   end
 
-  def create
-    user_facade = UserFacade.new
-    # require 'pry'; binding.pry
-    new_user_params = { name: 'John Doe', email: 'john@example.com', password: 'password123' }
+  # def create
+  #   user_facade = UserFacade.new
+  #   # require 'pry'; binding.pry
+  #   new_user_params = { }
 
-    # Create the user using the facade
-    user_facade.create_user(new_user_params)
+  #   # Create the user using the facade
+  #   user_facade.create_user(new_user_params)
 
-    redirect_to user_path(@user.id)
-  end
+  #   redirect_to user_path(@user.id)
+  # end
 end
