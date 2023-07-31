a2b1b1ac88b7cf0c29b011eec68d1a5c82ef935d
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-31 09:59:13 -0600

    add before_action callback for find_user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   before_action :require_login, only: :show
+  before_action :find_user, only: :show
 
   def show
+    # require 'pry'; binding.pry
+    @facade = UserFacade.new(params)
+  end
 
+  def create
+    user_facade = UserFacade.new
+    # require 'pry'; binding.pry
+    new_user_params = { name: 'John Doe', email: 'john@example.com', password: 'password123' }
+
+    # Create the user using the facade
+    user_facade.create_user(new_user_params)
+
+    redirect_to user_path(@user.id)
   end
 end
