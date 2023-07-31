acf36d76b2e262f124ae055f01224f5d0016951a
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-31 10:00:04 -0600

    add require login and find_user callbacks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :require_login
+  helper_method :find_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,5 +13,14 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "You must be logged in, to access the page you requested."
       redirect_to root_path
     end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = "User not found."
+    redirect_to root_path
   end
 end
