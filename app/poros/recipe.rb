36ab78e2eb53594fcac97392dc653b29cd374902
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-31 17:16:01 -0600

    add Recipe poros with .initialize method.

---
+++ app/poros/recipe.rb
@@ -0,0 +1,23 @@
+class Recipe
+  attr_reader :name,
+              :instructions,
+              :image_url,
+              :cook_time,
+              :public_status,
+              :source_name,
+              :source_url,
+              :user_submitted,
+              :api_id
+
+  def initialize(data)
+    @name = data[:name]
+    @instructions = data[:instructions]
+    @image_url = data[:image_url]
+    @cook_time = data[:cook_time]
+    @public_status = data[:public_status]
+    @source_name = data[:source_name]
+    @source_url = data [:source_url]
+    @user_submitted = data[:user_submitted]
+    @api_id = data[:api_id]
+  end
+end
