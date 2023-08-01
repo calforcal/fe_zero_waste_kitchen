a006a920f3cf4810015232597c72bd701b154e1a
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-01 13:14:00 -0400

    commented out code in the search controller to use

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def index
-    # @items = Facade.items
+    #  require 'pry'; binding.pry
+    # Get the search query from the params (assuming it's passed as 'search' parameter)
+    # query = params[:search]
+
+    # Call the facade function to search for recipes
+    # @recipes = KitchenFacade.search_recipes(query)
   end
 end
