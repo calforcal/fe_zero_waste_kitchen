d8ffabc8ff8836857a3f383a79d312a314dd5dc9
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-01 20:32:03 -0600

    wip recipe search testing

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -5,11 +5,14 @@ RSpec.describe "Recipe Search", type: :feature do
     it "can search by name of recipe and see results" do 
       visit search_index_path
 
-      fill_in "recipeNameInput", with: "potato"
-      click_button "Search" 
-
+      fill_in "#recipeNameInput", with: "potato"
+      # click_button "Search"
+      find(:button, id: "nameSearchButton").click
+      
+      save_and_open_page
       expect(current_path).to eq(search_index_path)
-      expect(page).to have_content("")
+      expect(page).to have_content("Oven Potatoes")
+      expect(page).to have_content("Old-Fashioned Potato Salad")
     end
   end
 end
